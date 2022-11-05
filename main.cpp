7b5f4e64a29c4fcfbfd6d393777dacc2e40b6391
ammarkandil <112958115+ammarkandil@users.noreply.github.com>
2022-11-05 16:16:23 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "HashTable.hpp"
+using namespace std;
+
+void hashtable_insert(HashTable(), char* num, char* value);
+
+
+int main (){
+    HashTable hash;
+       string a;
+       int b,c,d;
+       while (1) {
+          cout<<"1.Insert element "<<endl;
+          cout<<"2.Search for element "<<endl;
+          cout<<"3.Remove element "<<endl;
+          cout<<"Choose from the options: ";
+          cin>>c;
+          switch(c) {
+             case 1:
+                cout<<"Enter name: ";
+                cin>>a;
+                cout<<"Enter age ";
+                cin>>b;
+                cout<<"Enter salary ";
+                cin>>c;
+                cout<<"Enter experience in years ";
+                cin>>d;
+              
+             break;
+}
+
